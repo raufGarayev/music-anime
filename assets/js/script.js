71084acf14dd26c85d2af1c18d2f4b67494fdbf9
GARAYEV-PG <rafkagarayev@gmail.com>
2022-09-11 18:46:42 +0400

    many changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,13 @@ const progressCont = document.getElementById('progress-container')
 const img = document.querySelector('img')
 const imgDiv = document.querySelector('.player-img')
 
+const barBtn = document.getElementById('burger')
+const list = document.querySelector('.overlay-content')
+
+
+
+let currentPlaying
+
 const songs = [
     {
         name: 'Biri var',
@@ -29,7 +36,7 @@ const songs = [
     },
     {
         name: 'Slave',
-        displayName: 'Slave',
+        displayName: 'I wanna be your slave',
         artist: 'Maneskin',
         img: 'maneskin.jpg'
     }
@@ -44,6 +51,16 @@ function playSong() {
     imgDiv.classList.add('active')
     
     music.play()
+
+    const listDis = document.querySelectorAll('#list-display')
+    if(isOpened) {
+        for(let i=0; i<listDis.length; i++) {
+            if(listDis[i].innerText == currentPlaying)
+                listDis[i].parentElement.parentElement.style.color = "green"
+            else
+                listDis[i].parentElement.parentElement.style.color = "#fff"
+        }
+    }
 }
 
 function pauseSong() {
@@ -65,6 +82,7 @@ function loadSong(song) {
     artistEl.textContent = song.artist
     music.src = `./../../musics/${song.name}.mp3`
     img.setAttribute('src', `./assets/img/${song.img}`)
+    currentPlaying = song.displayName
 }
 
 let songIndex = 0
@@ -101,8 +119,6 @@ function updateProgress(e) {
         const progressPercent = (currentTime/duration)*100
         progress.style.width = `${progressPercent}%`
 
-        //calculate display
-
         const durationMins = Math.floor(duration/60)
         let durationSecs =  Math.floor(duration%60)
 
@@ -136,3 +152,70 @@ function setProgress(e) {
 
 music.addEventListener('timeupdate', updateProgress)
 progressCont.addEventListener('click', setProgress)
+
+let isOpened = false
+
+function closeNav() {
+    isOpened=false
+    document.getElementById("myNav").style.width = "0%";
+    list.innerHTML = ""
+}
+
+function openNav() {
+    isOpened = true
+    document.getElementById("myNav").style.width = "20%";
+
+    for(let i=0; i<songs.length; i++) {
+        list.innerHTML += `<div class="list-item"><p id="${i}" class="ft-play">${i+1}</p><img src="./assets/img/${songs[i].img}" alt=""><div id="list-item_right"><p id="list-display">${songs[i].displayName}</p><p>${songs[i].artist}</p></div></div>`
+    }
+
+    const listsong = document.querySelectorAll('.ft-play')
+    listsong.forEach(btn => {
+        btn.addEventListener('click', playListSong)
+    })
+
+    listsong.forEach(btn => {
+        btn.addEventListener('mouseover', changeIcon)
+    })
+
+    listsong.forEach(btn => {
+        btn.addEventListener('mouseleave', backIcon)
+    })
+}
+
+function playListSong(e) {
+    const clickedEl = e.target
+
+    loadSong(songs[clickedEl.id])
+    playSong()
+
+    const listsong = document.querySelectorAll('.ft-play')
+    for(let i = 0; i < listsong.length; i++) {
+        listsong[i].parentElement.style.color = "#fff"
+    }
+
+    clickedEl.parentElement.parentElement.parentElement.style.color = "green"
+}
+
+function changeIcon(e) {
+    const hovered = e.target
+    if(hovered.classList.contains('ft-play'))
+        hovered.innerHTML = `<p><i class="fas fa-play" id="${hovered.id}" title="Play"></i></p>` 
+}
+
+function backIcon(e) {
+    const lefthover = e.target
+    if(lefthover.classList.contains('ft-play')) {
+        lefthover.innerHTML = Number(lefthover.id)+1
+    }
+}
+
+barBtn.addEventListener('click', function(e) {
+    isOpened ? closeNav() : openNav()
+})
+
+const X = document.querySelector('.closebtn')
+
+X.addEventListener('click', function(e) {
+    closeNav()
+})
